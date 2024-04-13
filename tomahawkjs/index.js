e1f3ad19622b527a6eaf3d74ebc0f2c343545317
André Rodrigues <79713707+Andre-Rodrigues-Dev-Web@users.noreply.github.com>
2024-04-13 19:36:22 -0300

    feat: adicionado gitignore

--- tomahawkjs/index.js
+++ tomahawkjs/index.js
@@ -1,40 +1,52 @@
-#!/usr/bin/env node
-
-const { exec } = require('child_process');
+const { execSync } = require('child_process');
 const { program } = require('commander');
+const inquirer = require('inquirer');
+const path = require('path');
 
 program
   .option('-l, --layout <layout>', 'Specify the layout type: react or angular')
   .parse(process.argv);
 
-const layout = program.layout.toLowerCase();
+const layout = program.layout.toLowerCase() || 'react';
+
+if (!layout) {
+  console.error('Missing layout option. Please specify either "react" or "angular".');
+  process.exit(1);
+}
 
 if (layout === 'react') {
   console.log('Initializing React project...');
-  exec('npx create-react-app my-react-app', (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      console.error(`Error: ${stderr}`);
-      return;
-    }
-    console.log(`Success: ${stdout}`);
-  });
+
+  inquirer
+    .prompt([
+      {
+        type: 'checkbox',
+        name: 'options',
+        message: 'Select additional options:',
+        choices: [
+          { name: 'Styled Components' },
+          { name: 'Axios' },
+          { name: 'React Router DOM' }
+        ]
+      }
+    ])
+    .then(answers => {
+      const selectedOptions = answers.options;
+      const optionsString = selectedOptions.join(' ');
+
+      console.log('Installing dependencies...');
+      const projectPath = path.resolve('my-react-app');
+      execSync(`npx create-react-app ${projectPath} && cd ${projectPath} && npm install ${optionsString}`);
+      console.log('Success!');
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      process.exit(1);
+    });
 } else if (layout === 'angular') {
   console.log('Initializing Angular project...');
-  exec('npx @angular/cli new my-angular-app', (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      console.error(`Error: ${stderr}`);
-      return;
-    }
-    console.log(`Success: ${stdout}`);
-  });
+  execSync('npx @angular/cli new my-angular-app');
+  console.log('Success!');
 } else {
-  console.error('Invalid layout type. Please specify either "react" or "angular".');
+    console.log('Erro de execução!');
 }
